f303ae20ebc38d3ef0757c263071474fd3b1f729
springlee <939941249@qq.com>
2017-11-17 16:35:45 +0800

    微信接入验证文件

---
+++ wx_checkout.php
@@ -0,0 +1,26 @@
+<?php
+/**
+  * 微信接入验证
+  */ 
+
+// TOKEN定义
+define('TOKEN', 'spring');
+
+//参数接收
+$signature = $_GET['signature'];
+$timestamp = $_GET['timestamp'];
+$nonce = $_GET['nonce'];
+$echostr = $_GET['echostr'];
+
+// 字典排序
+$tmpArr = array(TOKEN, $timestamp, $nonce);
+sort($tmpArr, SORT_STRING);
+
+//拼接加密
+$tmpStr = implode($tmpArr);
+$tmpStr = sha1($tmpStr);
+
+//比对确认,成功时返回信息
+if ($tmpStr == $signature) {
+	echo $echostr;
+}
